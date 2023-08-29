895b3151a6bbde8c1a6ac4f4cdb1b23faaf318a7
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2023-08-29 21:13:14 +0900

    [TEST] Ragdoll 부하 테스트

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class PlayerAnimationController : MonoBehaviour
 {
